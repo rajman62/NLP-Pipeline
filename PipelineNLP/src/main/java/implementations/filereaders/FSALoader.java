a913f058bc88ed9830f827b9b297f7f970b749d5
marc <marc_adams@hotmail.fr>
2017-11-03 12:05:37 +0100

    FSAloader saveToFile implemented

--- PipelineNLP/src/main/java/implementations/filereaders/FSALoader.java
+++ PipelineNLP/src/main/java/implementations/filereaders/FSALoader.java
@@ -3,18 +3,16 @@ package implementations.filereaders;
 import dk.brics.automaton.Automaton;
 import dk.brics.automaton.RegExp;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.LinkedList;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class FSALoader {
     private Pattern textExtension = Pattern.compile("^.*\\.txt$");
     private Pattern serializedExtension = Pattern.compile("^.*\\.ser$");
     private Pattern ignoreLineInTextFile = Pattern.compile("^((\\s*)|(#.*))$");
-
+    private Pattern extractRegex = Pattern.compile("^<(.*)>\\s*$");
 
     public Automaton loadFromFile(String path) throws Exception {
         if (textExtension.matcher(path).matches()) {
@@ -23,9 +21,23 @@ public class FSALoader {
             return Automaton.load(new FileInputStream(path));
         }
 
-        throw new IOException("Unrecognized file format for:" + path);
+        throw new IOException("Unrecognized file format for: " + path);
     }
 
+    public void saveToFile(Automaton fsa, String path) throws Exception {
+        if (textExtension.matcher(path).matches()) {
+            throw new UnsupportedOperationException(
+                    "Cannot save automaton to txt file ("
+                            + path +
+                            "), an automaton can potentially store an infinite number of words " +
+                            "and regexes are lost during the reduce operation.");
+        } else if (serializedExtension.matcher(path).matches()) {
+            fsa.store(new FileOutputStream(path));
+        } else {
+            throw new IOException("Unrecognized file format for: " + path);
+        }
+    }
+    
     private Automaton txtFileLoader(String path) throws Exception {
         BufferedReader input = new BufferedReader(new FileReader(path));
         LinkedList<RegExp> regexList = new LinkedList<>();
@@ -41,7 +53,11 @@ public class FSALoader {
                 continue;
 
             try {
-                regexList.add(new RegExp(line));
+                Matcher reg = extractRegex.matcher(line);
+                if (reg.matches())
+                    regexList.add(new RegExp(reg.group(1)));
+                else
+                    regexList.add(new RegExp(line));
             } catch (IllegalArgumentException exp) {
                 throw new IllegalArgumentException("In file " + path + " line " + lineNumber + ": " + exp.getMessage());
             }
--- PipelineNLP/src/test/java/filereaders/LoadAndStoreFSAs.java
+++ PipelineNLP/src/test/java/filereaders/LoadAndStoreFSAs.java
@@ -8,18 +8,41 @@ import static org.junit.Assert.assertEquals;
 
 
 public class LoadAndStoreFSAs {
+    static final String TEST_PATH = "src/test/java/filereaders/";
+
     @Test
     public void readFile() throws Exception {
         FSALoader fsaLoader = new FSALoader();
-        Automaton fsa = fsaLoader.loadFromFile("src/test/java/filereaders/word_fsa_example.txt");
+        Automaton fsa = fsaLoader.loadFromFile(TEST_PATH + "word_fsa_example.txt");
         assertEquals(false, fsa.run("wo"));
         assertEquals(true, fsa.run("world"));
         assertEquals(true, fsa.run("john.doe@mail.com"));
+        assertEquals(true, fsa.run(" t "));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void readFileWithError() throws Exception {
         FSALoader fsaLoader = new FSALoader();
-        Automaton fsa = fsaLoader.loadFromFile("src/test/java/filereaders/word_fsa_error.txt");
+        Automaton fsa = fsaLoader.loadFromFile(TEST_PATH + "word_fsa_error.txt");
+    }
+
+    @Test
+    public void saveAndLoadFromSerFile() throws Exception {
+        FSALoader fsaLoader = new FSALoader();
+        Automaton fsa = fsaLoader.loadFromFile(TEST_PATH + "word_fsa_example.txt");
+        fsaLoader.saveToFile(fsa, TEST_PATH + "word_fsa_example.ser");
+
+        Automaton loadedFsa = fsaLoader.loadFromFile(TEST_PATH + "word_fsa_example.ser");
+        assertEquals(false, loadedFsa.run("wo"));
+        assertEquals(true, loadedFsa.run("world"));
+        assertEquals(true, loadedFsa.run("john.doe@mail.com"));
+        assertEquals(true, loadedFsa.run(" t "));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void saveFileWithError() throws Exception {
+        FSALoader fsaLoader = new FSALoader();
+        Automaton fsa = fsaLoader.loadFromFile(TEST_PATH + "word_fsa_example.txt");
+        fsaLoader.saveToFile(fsa, "src/test/java/filereaders/word_fsa_save.txt");
     }
 }
